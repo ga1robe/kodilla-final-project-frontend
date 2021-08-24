ffb1aab629059d8ca4513095cb54861629e5cdf7
Robert Gajewski <robert.gajewski@nask.pl>
2021-08-24 18:04:09 +0200

    first commit

---
+++ src/main/java/com/kodilla/servicefrontend/ServiceFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.servicefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/servicefrontend/ServiceFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.servicefrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFrontendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
